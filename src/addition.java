f67ffd2fe7d70df29052e11300639cad3336cdac
Kirthioswal <oswalkirti2@gmail.com>
2025-05-08 00:42:33 +0530

    changes done and created helloworld java program

--- src/addition.java
+++ src/addition.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class addition {
+    public static  void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter 1st number:");
+        int a=s.nextInt();
+        System.out.println("Enter 2nd number:");
+        int b=s.nextInt();
+        int sum=a+b;
+
+        System.out.println("sum of 2 digits :"+sum);
+
+    }
 }
