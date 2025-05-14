0d634d52597eaaa9985b7375635ff0d88c38675e
Kirthioswal <oswalkirti2@gmail.com>
2025-05-14 21:52:00 +0530

    New code changes made-variables

---
+++ src/ex_01_java_basics_par2/javavariables.java
@@ -0,0 +1,17 @@
+package ex_01_java_basics_par2;
+
+
+public class javavariables {
+    public static void main(String[] args) {
+        int _a = 45;
+        int are_$ = 54;
+        int $ = 60;
+        int vaiable_name_descritpion_long_format_name_12345_657_Lognhf_ = 123;
+        /* it wont give u error if u use _ as identifier but it wont give u output of that indentifier
+        int _ = 34; */
+        System.out.println(_a);
+        System.out.println(are_$);
+        System.out.println(vaiable_name_descritpion_long_format_name_12345_657_Lognhf_);
+        System.out.println($);
+    }
+}
