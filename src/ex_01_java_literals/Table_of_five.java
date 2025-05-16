c5a00ba5a0fe391cc396600a64fd63089977126b
Kirthioswal <oswalkirti2@gmail.com>
2025-05-16 23:45:10 +0530

    New changes made- wrote program for literals

--- src/ex_01_java_literals/Table_of_five.java
+++ src/ex_01_java_literals/Table_of_five.java
@@ -16,8 +16,18 @@ public class Table_of_five {
                  n*seq[8],
                  n*seq[9]
         };
-        System.out.println(+n+"*"+seq[0]+"="+result[0]);
-        System.out.println(+n+"*"+seq[1]+"="+result[1]);
+        System.out.printf("%d * %d = %d\n",n ,seq[0],  result[0]);
+        System.out.printf("%d * %d = %d\n",n ,seq[1], result[1]);
+        System.out.printf("%d * %d = %d\n",n ,seq[2], result[2]);
+        System.out.printf("%d * %d = %d\n",n ,seq[3], result[3]);
+        System.out.printf("%d * %d = %d\n",n ,seq[4], result[4]);
+        System.out.printf("%d * %d = %d\n",n ,seq[5], result[5]);
+        System.out.printf("%d * %d = %d\n",n ,seq[6], result[6]);
+        System.out.printf("%d * %d = %d\n",n ,seq[7], result[7]);
+        System.out.printf("%d * %d = %d\n",n ,seq[8], result[8]);
+        System.out.printf("%d * %d = %d\n",n ,seq[9], result[9]);
+
+     /* System.out.println(+n+"*"+seq[1]+"="+result[1]);
         System.out.println(+n+"*"+seq[2]+"="+result[2]);
         System.out.println(+n+"*"+seq[3]+"="+result[3]);
         System.out.println(+n+"*"+seq[4]+"="+result[4]);
@@ -25,6 +35,6 @@ public class Table_of_five {
         System.out.println(+n+"*"+seq[6]+"="+result[6]);
         System.out.println(+n+"*"+seq[7]+"="+result[7]);
         System.out.println(+n+"*"+seq[8]+"="+result[8]);
-        System.out.println(+n+"*"+seq[9]+"="+result[9]);
+        System.out.println(+n+"*"+seq[9]+"="+result[9]);  */
     }
 }
