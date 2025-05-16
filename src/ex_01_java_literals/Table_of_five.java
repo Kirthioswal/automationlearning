ecec6a86fa5b0e099a96b5d3ccea677ddf4a314a
Kirthioswal <oswalkirti2@gmail.com>
2025-05-16 23:34:01 +0530

    New changes made- wrote program for literals

---
+++ src/ex_01_java_literals/Table_of_five.java
@@ -0,0 +1,30 @@
+package ex_01_java_literals;
+
+public class Table_of_five {
+    public static void main(String[] args){
+        int n=5;
+        int[] seq={1,2,3,4,5,6,7,8,9,10};
+        int[] result = {
+                 n*seq[0],
+                 n*seq[1],
+                 n*seq[2],
+                 n*seq[3],
+                 n*seq[4],
+                 n*seq[5],
+                 n*seq[6],
+                 n*seq[7],
+                 n*seq[8],
+                 n*seq[9]
+        };
+        System.out.println(+n+"*"+seq[0]+"="+result[0]);
+        System.out.println(+n+"*"+seq[1]+"="+result[1]);
+        System.out.println(+n+"*"+seq[2]+"="+result[2]);
+        System.out.println(+n+"*"+seq[3]+"="+result[3]);
+        System.out.println(+n+"*"+seq[4]+"="+result[4]);
+        System.out.println(+n+"*"+seq[5]+"="+result[5]);
+        System.out.println(+n+"*"+seq[6]+"="+result[6]);
+        System.out.println(+n+"*"+seq[7]+"="+result[7]);
+        System.out.println(+n+"*"+seq[8]+"="+result[8]);
+        System.out.println(+n+"*"+seq[9]+"="+result[9]);
+    }
+}
