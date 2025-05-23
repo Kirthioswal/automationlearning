a5772c93429115f443ff00fd6878a21a0bb1f622
Kirthioswal <oswalkirti2@gmail.com>
2025-05-23 23:41:45 +0530

    New changes made- wrote program for triangle_classifying--- 23 may 2025

---
+++ src/Java_conditional_statements/Triangle_classifier.java
@@ -0,0 +1,24 @@
+package Java_conditional_statements;
+
+import java.util.Scanner;
+
+public class Triangle_classifier {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter side 1 :");
+        float s1 = s.nextFloat();
+        System.out.println("Enter side 2 :");
+        float s2 = s.nextFloat();
+        System.out.println("Enter side3 :");
+        float s3 = s.nextFloat();
+
+        if(s1==s2 && s2==s3) {
+            System.out.println("Its an Equilateral Triangle");
+        }
+        else if (s1==s2 || s2==s3 || s1==s3){
+            System.out.println("Its an Isosceles Triangle");
+        }
+        else
+            System.out.println("Its an Scalene Triangle");
+    }
+}
