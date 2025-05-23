07501c4acec5055b87eef866b9a22f6b658be5b9
Kirthioswal <oswalkirti2@gmail.com>
2025-05-23 22:30:25 +0530

    New changes made- wrote program for ternary--- 23 may 2025

--- src/Java_operators/even_odd.java
+++ src/Java_operators/even_odd.java
@@ -3,19 +3,19 @@ package Java_operators;
 import java.util.Scanner;
 
 public class even_odd {
+
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in)  ;
+
+        Scanner s = new Scanner(System.in);
         System.out.println("Enter a :");
-        int a=s.nextInt();
-        System.out.println("Enter b :");
-        int b=s.nextInt();
+        int a = s.nextInt();
 
-        if(a%b==0){
-            System.out.println("This is a even number");
-        }
-        else {
-            System.out.println("This is a odd number");
-        }
 
+        String result = (a % 2 == 0) ? "Even" : "Odd"; //Ternary operator
+        System.out.println(result);
     }
 }
+
+
+
+
