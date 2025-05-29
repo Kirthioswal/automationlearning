92142b5c9bb7376fc0c2177e8d495e590634bbcf
Kirthioswal <oswalkirti2@gmail.com>
2025-05-29 23:00:57 +0530

    New changes made- wrote program for switch statements--- 25 may 2025

--- src/Java_conditional_statements/Switch_automation_example.java
+++ src/Java_conditional_statements/Switch_automation_example.java
@@ -9,30 +9,30 @@ public class Switch_automation_example {
         String browser=s.next();
         browser=browser.toLowerCase();
         switch (browser){
-            case "chrome" :
+            case "chrome" -> {
                 System.out.println("Starting a chrome browser : " );
                 System.out.println("....... " );
                 System.out.println("TC01 " );
                 System.out.println("TC02 " );
                 System.out.println("TC03 " );
-                break;
-            case "edge" :
+            }
+            case "edge" -> {
                 System.out.println("Starting a Edge browser : " );
                 System.out.println("....... " );
                 System.out.println("TC01 " );
                 System.out.println("TC02 " );
                 System.out.println("TC03 " );
-                break;
-            case "firefox" :
+            }
+            case "firefox"->{
                 System.out.println("Starting a Firefox browser : " );
                 System.out.println("....... " );
                 System.out.println("TC01 " );
                 System.out.println("TC02 " );
                 System.out.println("TC03 " );
-                break;
-            default:
+            }
+            default ->
                 System.out.println("Invalid browser , Please enter a Valid browser : " );
-                break;
+
 
             }
         }
